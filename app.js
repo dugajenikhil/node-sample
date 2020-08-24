201af17cc47efb2e85d217a8de7b19c033ea69af
nikhil dugaje <33528273+dugajenikhil@users.noreply.github.com>
2020-08-24 15:34:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World from NodeJs Application deployed on ECS');
+  res.send('Hello World from' + process.env.user + 'Application deployed on ECS');
 });
 app.listen(80, function () {
   console.log('Example app listening on port 80!');

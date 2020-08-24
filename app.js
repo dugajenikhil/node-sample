9f825485db2702c4fd07676d04438f745e64966b
dugajenikhil <nikhil.dugaje@bnedloudcloud.com>
2020-08-24 16:32:12 +0530

    env

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var dotenv = require('dotenv').config();
+
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World from' + process.env.user + 'Application deployed on ECS');
+  res.send('Hello World from ' + process.env.USER + ' Application deployed on ECS');
 });
 app.listen(80, function () {
   console.log('Example app listening on port 80!');

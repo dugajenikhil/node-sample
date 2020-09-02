3ee340ef569cd27da25cf5c62176cfec917afe43
nikhil dugaje <33528273+dugajenikhil@users.noreply.github.com>
2020-09-02 20:10:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var dotenv = require('dotenv').config();
 
 var app = express();
 app.get('/', function (req, res) {
+  console.log('log: ', {
+    fruit: "Apple",
+    size: "Large",
+    color: "Red"
+  }
   res.send('Hello World from ' + process.env.USER + ' Application deployed on ECS');
 });
 app.listen(80, function () {

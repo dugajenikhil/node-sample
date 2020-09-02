e027b2fe48f757ce32ffe2e450411564883ddf8a
nikhil dugaje <33528273+dugajenikhil@users.noreply.github.com>
2020-09-03 00:52:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ var dotenv = require('dotenv').config();
 
 var app = express();
 app.get('/', function (req, res) {
-  console.log('log: ', {
+  console.log({
     fruit: "Apple",
     size: "Large",
     color: "Red"
   })
-  console.log('log: ', {
+  console.log({
     fruit: "Orange",
     size: "Large",
     color: "Orange"
   })
-  console.log('log: ', {
+  console.log(JSON.stringify({
     fruit: "Cherry",
     size: "Medium",
     color: "Red"
-  })
-  console.log('log: ', {
+  }))
+  console.log(JSON.stringify({
     fruit: "Berries",
     size: "Small",
     color: "Blue"
-  })
+  }))
   res.send('Hello World from ' + process.env.USER + ' Application deployed on ECS');
 });
 app.listen(80, function () {

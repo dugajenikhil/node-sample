55684feedd997d3cee7bd044554bc29daf871f50
nikhil dugaje <33528273+dugajenikhil@users.noreply.github.com>
2020-09-03 18:26:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,14 @@ var dotenv = require('dotenv').config();
 
 var app = express();
 app.get('/', function (req, res) {
-  console.log({
-    fruit: "Apple",
-    size: "Large",
-    color: "Red"
-  })
-  console.log({
-    fruit: "Orange",
-    size: "Large",
-    color: "Orange"
-  })
-  console.log(JSON.stringify({
-    fruit: "Cherry",
-    size: "Medium",
-    color: "Red"
-  }))
-  console.log(JSON.stringify({
-    fruit: "Berries",
-    size: "Small",
-    color: "Blue"
-  }))
+  let obj = {
+    name:'nikhil',
+    innerObj: {
+      id: 50 
+    }
+  }
+  console.log(JSON.stringify(obj));
+
   res.send('Hello World from ' + process.env.USER + ' Application deployed on ECS');
 });
 app.listen(80, function () {

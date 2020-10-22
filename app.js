39ab0fd1a22224f113e80422685935c56e579778
nikhil dugaje <33528273+dugajenikhil@users.noreply.github.com>
2020-10-22 14:25:09 +0530

    jenkins-test-2-25

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var app = express();
 app.get('/', function (req, res) {
   require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log('addr: '+add);
-    res.send('<h2>App running....................... IP Address: '+add+'</h2>');
+    res.send('<h2>App running..... IP Address: '+add+'</h2>');
+  })
+})
+
+app.get('/jenkins', function (req, res) {
+    res.send('Jenkins deploy successful');
   })
 })
 

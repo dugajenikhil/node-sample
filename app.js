7a41683d7c8636a0d619105944a259e870dcdcf2
nikhil dugaje <33528273+dugajenikhil@users.noreply.github.com>
2020-10-22 14:32:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.get('/', function (req, res) {
   require('dns').lookup(require('os').hostname(), function (err, add, fam) {
     console.log('addr: '+add);
-    res.send('<h2>App running..... IP Address: '+add+'</h2>');
+    res.send('<h2>App running.... IP Address: '+add+'</h2>');
   })
 })
 
